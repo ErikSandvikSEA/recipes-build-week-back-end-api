7d1e4beceeb012477b0ef68eacc9dc9587c95238
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-23 12:07:38 -0700

    PUT and DELETE functionality working

--- api/recipes/recipesRouter.js
+++ api/recipes/recipesRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Recipes = require('../apiModel')
 const { requiredProperty } = require('../middleware')
+const { findById } = require('../apiModel')
 
 
 router.get(
@@ -68,5 +69,73 @@ router.post(
      }
 )
 
+router.put(
+     '/:id', 
+     requiredProperty('title'),
+     requiredProperty('user'),
+     requiredProperty('user_id'),
+     requiredProperty('category'),
+     requiredProperty('instructions'),
+     requiredProperty('ingredients'),
+     (req, res) => {
+          const recipeId = req.params.id
+          const updatedRecipe =  req.body
+          Recipes.updateRecipe(updatedRecipe, recipeId)
+               .then(recipe => {
+                    res.status(201).json({
+                         message: 'Recipe successfully updated!',
+                         updatedRecipe: recipe
+                    })
+               })
+               .catch(err => {
+                    console.log(err)
+                    res.status(500).json({
+                         errorMessage: err.message,
+                         message: 'Error occurred during update'
+                    })
+               })
+})
+
+router.delete(
+     '/:id',
+     validateId,
+     async (req, res) => {
+          const recipeId = req.params.id
+          const recipeToDelete = await findById('recipesList', req.params.id)
+          Recipes.deleteRecipe(recipeId)
+               .then(deleted => {
+                    res.status(200).json({
+                         message: 'Successfully deleted',
+                         deletedRecipe: recipeToDelete
+                    })
+               })
+               .catch(err => {
+                    res.status(500).json({
+                         message: 'Error occurred while deleting',
+                         errorMessage: err.message
+                    })
+               })
+     }
+)
+
+function validateId(req, res, next){
+     const validatedId = req.params.id
+     Recipes.findById('recipesList', validatedId)
+          .then(recipe => {
+               if(recipe) {
+                    next()
+               } else {
+                    res.status(404).json({
+                         message: `Could not locate recipe ID: ${validatedId}`
+                    })
+               }
+          })
+          .catch(err => {
+               res.status(500).json({
+                    message: `Could not locate recipe ID: ${validatedId}`,
+                    error: err.message
+               })
+          })
+}
 
 module.exports = router
